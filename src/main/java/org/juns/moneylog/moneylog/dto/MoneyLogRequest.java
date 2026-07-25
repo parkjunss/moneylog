bcd5649dba9c2207940c7264b2da0ad565b11476
jun <wnstjd117@gmail.com>
2026-07-25 20:08:14 +0900

    fix:valid 추가

--- src/main/java/org/juns/moneylog/moneylog/dto/MoneyLogRequest.java
+++ src/main/java/org/juns/moneylog/moneylog/dto/MoneyLogRequest.java
@@ -1,11 +1,24 @@
 package org.juns.moneylog.moneylog.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 public record MoneyLogRequest(
-        String username,
+        @NotBlank
         String title,
+
+        @NotBlank
         String description,
+
+        @NotNull
+        @Positive
         Long money,
+
+        @NotBlank
         String date,
+
+        @NotBlank
         String category
 ) {
 }
